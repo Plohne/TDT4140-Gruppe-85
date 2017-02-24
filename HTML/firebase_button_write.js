bc70a46a9b5ee8f3c94a4b6cba8114ae288f2acf
Petter Lohne <plohne95@gmail.com>
2017-02-24 12:34:53 +0100

    Laget spm som oppdateres live

--- HTML/firebase_button_write.js
+++ HTML/firebase_button_write.js
@@ -6,3 +6,39 @@ function writeButtonData(input){
     });
 }
 
+
+
+var q2 = document.getElementById("spm2");
+var q3 = document.getElementById("spm3");
+
+
+
+
+
+var spmRef = firebase.database().ref("spm");
+
+spmRef.on("child_added", function(snapshot, prevChildKey) {
+  var newPost = snapshot.val();
+    var q1 = document.getElementById("UNIQUEID");
+    q1.innerText += "\n" + "\n" +  newPost;
+
+});
+
+
+
+
+function askQuestion() {
+    var aQ = firebase.database().ref("spm");
+    var newQ = aQ.push();
+    var spmStilt = document.getElementById("studentSpm").value;
+    newQ.set(spmStilt);
+    document.getElementById("studentSpm").value = "";
+    
+    var spmID2 = newQ.getKey();
+    
+
+    
+    
+    
+   
+}
